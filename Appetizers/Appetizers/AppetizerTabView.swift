c18133e82e5affa60e6af3128137d074a171e638
Norbert Agoston <norbert.agst@gmail.com>
2024-02-19 13:21:41 +0200

    Add new project
    - create new project
    - add new basic navigation structure using a TabBar

---
+++ Appetizers/Appetizers/AppetizerTabView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Norbert Agoston on 19.02.2024.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            AppetizerListView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
+        }
+        .accentColor(Color("brandPrimary"))
+    }
+}
+
+#Preview {
+    AppetizerTabView()
+}
---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Norbert Agoston on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
