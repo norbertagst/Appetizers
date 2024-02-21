605b0dbbdcf15ae5363c1899890823f78faafe0d
Norbert Agoston <norbert.agst@gmail.com>
2024-02-21 18:20:26 +0200

    Create appetizer detail view
    - create a new detail view for a selected appetizer
    - link the appetizers list view with the detail view of a selected appetizer

--- Appetizers/Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -9,17 +9,30 @@ import SwiftUI
 
 struct AppetizerListView: View {
     @StateObject var viewModel = AppetizersListViewModel()
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            selectedAppetizer = appetizer
+                            isShowingDetail = true
+                        }
                 }
                 .navigationTitle("Appetizers")
+                .disabled(isShowingDetail)
             }
             .onAppear {
                 viewModel.getAppetizers()
+            }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            
+            if isShowingDetail {
+                AppetizerDetailView(appetizer: selectedAppetizer!,
+                                    isShowingDetail: $isShowingDetail)
             }
             
             if viewModel.isLoading {
